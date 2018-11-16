0be129119a068cea11ae33e7d88d5fdeef928a9b
Pooya Rostam <pooyarm@gmail.com>
2018-11-16 17:01:22 +0330

    dont show already added members

--- src/containers/add.js
+++ src/containers/add.js
@@ -22,7 +22,7 @@ class AddContainer extends Component {
         return (
             <AddMemberPresentation
                 addHandler={this.add}
-                users={this.props.users}
+                users={this.props.users.filter((user) => (this.props.members.indexOf(user.id) < 0))}
             />
         )
     }
@@ -30,7 +30,8 @@ class AddContainer extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-        users: state.users
+        users: state.users,
+        members: state.members
 	}
 }
 
